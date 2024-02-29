3096c15ae0afb54432575264803c2383d721fab8
marina <m.zagaynova@i-sys.ru>
2024-02-29 15:27:35 +0400

    develop

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,55 +1,85 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+
+import data
 import locators
 
 
-def test_from_lk_to_constructor():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://stellarburgers.nomoreparties.site/")
-    WebDriverWait(driver, 3).until(
-        expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance_button)))
-    driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
-    WebDriverWait(driver, 3).until(
-        expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance)))
-    driver.find_element(By.XPATH, locators.email_login).send_keys(locators.email_send)
-    driver.find_element(By.XPATH, locators.password_registration).send_keys(locators.password_send)
-    driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
-    WebDriverWait(driver, 3).until(
-        expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
-    driver.find_element(By.XPATH, locators.button_lk).click()
-    WebDriverWait(driver, 3).until(
-        expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.exit_button)))
-    driver.find_element(By.LINK_TEXT, locators.constructor).click()
-    WebDriverWait(driver, 3).until(
-        expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
-    assert driver.find_element(By.CLASS_NAME, locators.text_main).text == "Соберите бургер"
-    driver.quit()
+class Constructor:
+    @staticmethod
+    def test_from_lk_to_constructor(driver):
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance_button)))
+        driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance)))
+        driver.find_element(By.XPATH, locators.email_login).send_keys(data.email_login)
+        driver.find_element(By.XPATH, locators.password_registration).send_keys(data.password_send)
+        driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
+        driver.find_element(By.XPATH, locators.button_lk).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.exit_button)))
+        driver.find_element(By.LINK_TEXT, locators.constructor).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
+        assert driver.find_element(By.CLASS_NAME, locators.text_main).text == "Соберите бургер"
 
+    @staticmethod
+    def test_constructor_souses(driver):
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance_button)))
+        driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance)))
+        driver.find_element(By.XPATH, locators.email_login).send_keys(data.email_login)
+        driver.find_element(By.XPATH, locators.password_registration).send_keys(data.password_send)
+        driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
+        driver.find_element(By.LINK_TEXT, locators.constructor).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
+        driver.find_element(By.XPATH, locators.souses).click()
+        assert "tab_tab_type_current__2BEPc" in driver.find_element(By.XPATH, locators.souses).get_attribute("class")
 
-def test_constructor():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://stellarburgers.nomoreparties.site/")
-    WebDriverWait(driver, 3).until(
-        expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance_button)))
-    driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
-    WebDriverWait(driver, 3).until(
-        expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance)))
-    driver.find_element(By.XPATH, locators.email_login).send_keys(locators.email_send)
-    driver.find_element(By.XPATH, locators.password_registration).send_keys(locators.password_send)
-    driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
-    WebDriverWait(driver, 3).until(
-        expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
-    driver.find_element(By.LINK_TEXT, locators.constructor).click()
-    WebDriverWait(driver, 3).until(
-        expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
-    driver.find_element(By.XPATH, locators.souses).click()
-    assert driver.find_element(By.XPATH, locators.souses_test).text == "Соус с шипами Антарианского плоскоходца"
-    driver.find_element(By.XPATH, locators.fillings).click()
-    assert driver.find_element(By.XPATH, locators.fillings_test).text == "Филе Люминесцентного тетраодонтимформа"
-    driver.find_element(By.XPATH, locators.buns).click()
-    assert driver.find_element(By.XPATH, locators.buns_test).text == "Краторная булка N-200i"
-    driver.quit()
+    @staticmethod
+    def test_constructor_fillings(driver):
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance_button)))
+        driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance)))
+        driver.find_element(By.XPATH, locators.email_login).send_keys(data.email_login)
+        driver.find_element(By.XPATH, locators.password_registration).send_keys(data.password_send)
+        driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
+        driver.find_element(By.LINK_TEXT, locators.constructor).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
+        driver.find_element(By.XPATH, locators.fillings).click()
+        assert "tab_tab_type_current__2BEPc" in driver.find_element(By.XPATH, locators.fillings).get_attribute(
+            "class")
+
+    @staticmethod
+    def test_constructor_buns(driver):
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance_button)))
+        driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.entrance)))
+        driver.find_element(By.XPATH, locators.email_login).send_keys(data.email_login)
+        driver.find_element(By.XPATH, locators.password_registration).send_keys(data.password_send)
+        driver.find_element(By.CLASS_NAME, locators.entrance_button).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
+        driver.find_element(By.LINK_TEXT, locators.constructor).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, locators.text_main)))
+        driver.find_element(By.XPATH, locators.fillings).click()
+        driver.find_element(By.XPATH, locators.buns).click()
+        assert "tab_tab_type_current__2BEPc" in driver.find_element(By.XPATH, locators.buns).get_attribute(
+            "class")
